45067b655f06e6c6019932462f96500f0de7b107
liiisuuuu <uijk989@naver.com>
2026-03-17 00:03:56 +0900

    [feat] createVNode 0 처리 추가, normalizeVNode 조건 설정

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,4 +1,4 @@
 export function createVNode(type, props, ...children) {
-  children = children.flat(Infinity).filter((v) => v);
+  children = children.flat(Infinity).filter((v) => v || typeof v === "number");
   return { type, props, children };
 }
--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,20 @@
 export function normalizeVNode(vNode) {
+  /*
+    가상 노드 표준화된 형태로 변환
+    여러 타입의 입력 처리 -> 일관된 가상 노드 반환해서 DOM 조작/렌더링 과정에서 해당 데이터로 사용
+  */
+
+  if (
+    vNode === null ||
+    typeof vNode === "undefined" ||
+    typeof vNode === "boolean"
+  ) {
+    vNode = "";
+  } else if (typeof vNode === "number") {
+    vNode = vNode.toString();
+  } else if (typeof vNode?.type === "function") {
+    normalizeVNode(vNode.type(vNode?.props || {}));
+  }
+
   return vNode;
 }
